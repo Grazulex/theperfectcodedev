0c2e24602f2ccaea135e28708af0d0a36a655c18
jean-marc strauven <jms@grazulex.be>
2024-01-02 22:37:01 +0100

    add debug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,10 +13,7 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('local')) {
-            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-            $this->app->register(TelescopeServiceProvider::class);
-        }
+
     }
 
     /**
