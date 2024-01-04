9a44ab3cec92fee39e698b939404da4ae1533a9d
Jean-Marc Strauven <jms@grazulex.be>
2024-01-04 21:45:49 +0100

    add cache for top query

--- app/Http/Controllers/Pages/TopListController.php
+++ app/Http/Controllers/Pages/TopListController.php
@@ -11,17 +11,23 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 final class TopListController extends Controller
 {
     public function __invoke(Request $request): Application|View|\Illuminate\Foundation\Application|Factory
     {
-        $topPages = Page::withCount(['likes','followers', 'comments'])
-            ->with('user')
-            ->where('state', State::PUBLISHED)
-            ->orderBy('likes_count', 'desc')
-            ->limit(10)
-            ->get();
+        $topPages =  Cache::remember(
+            key: 'top',
+            ttl: now()->addDay(),
+            callback:  fn() => Page::withCount(['likes', 'followers', 'comments'])
+                ->with('user')
+                ->where('state', State::PUBLISHED)
+                ->orderBy('likes_count', 'desc')
+                ->limit(10)
+                ->get()
+        )
+        ;
 
         return view('pages.top-pages', ['topPages' => $topPages]);
     }
