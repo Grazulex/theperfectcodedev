af382445c020be7811ad506dd4ebfe78118172ea
jean-marc strauven <jms@grazulex.be>
2024-01-04 07:11:31 +0100

    update form

--- resources/views/components/pages/forms/create.blade.php
+++ resources/views/components/pages/forms/create.blade.php
@@ -2,51 +2,35 @@
     @csrf
     <div class="mt-4">
         <fieldset>
-            <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Code Improvement Title</label>
-            <input type="text" name="title" id="title" value="{{ old('title') }}" aria-describedby="title-explanation" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+            <x-label for="title" value="Code Improvement Title" />
+            <x-input id="title" class="block w-full mt-1" type="text" name="title" :value="old('title')" aria-describedby="title-explanation" />
             <p id="title-explanation" class="text-gray-600 text-xs italic">Please enter a concise and descriptive title for the code you wish to optimize. The title should capture the essence of the problem or desired enhancement in a few words. Example: 'Optimizing Sorting Loop for Increased Efficiency'.</p>
-            @if ($errors->has('title'))
-                @foreach ($errors->get('title') as $error)
-                    <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $error }}</p>
-                @endforeach
-            @endif
+            <x-input-error for="title" class="mt-2" />
         </fieldset>
     </div>
     <div class="mt-4">
         <fieldset>
-            <label for="resume" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Code Summary</label>
+            <x-label for="resume" value="Code summary" />
             <textarea name="resume" id="resume" rows="3" aria-describedby="resume-explanation" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">{{ old('resume') }}</textarea>
             <p id="resume-explanation" class="text-gray-600 text-xs italic">Please provide a brief summary describing the code you aim to enhance. This summary should encompass key aspects of the code, such as its function, weaknesses, or specific areas needing particular attention. Example: 'Summary of the current sorting script highlighting inefficiencies in handling large datasets'.</p>
-            @if ($errors->has('resume'))
-                @foreach ($errors->get('resume') as $error)
-                    <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $error }}</p>
-                @endforeach
-            @endif
+            <x-input-error for="resume" class="mt-2" />
         </fieldset>
     </div>
     <div class="mt-4">
         <fieldset>
-            <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Code Description</label>
+            <x-label for="description" value="Code Description" />
             <textarea name="description" id="description" rows="5" aria-describedby="description-explanation" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">{{ old('description') }}</textarea>
             <p id="description-explanation" class="text-gray-600 text-xs italic">Use this space to provide an in-depth description of the code in question. Describe its purpose, structure, any existing issues, and the desired enhancements extensively. Include relevant technical details and any additional information that can aid in understanding the context. Example: 'A comprehensive overview of the current sorting script, highlighting algorithmic gaps and suggestions to enhance efficiency by leveraging optimized data structures'.</p>
-            @if ($errors->has('description'))
-                @foreach ($errors->get('description') as $error)
-                    <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $error }}</p>
-                @endforeach
-            @endif
+            <x-input-error for="description" class="mt-2" />
         </fieldset>
     </div>
     <div class="mt-4">
         <fieldset>
-            <label for="editor" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Code Section</label>
+            <x-label for="editor" value="Code Section" />
             <input type="hidden" name="code" id="code">
             <div name="code" id="editor" rows="5" aria-describedby="code-explanation" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">{{ old('code') }}</div>
             <p id="code-explanation" class="text-gray-600 text-xs italic">Please enter or paste the code you wish to enhance here. Ensure that the entirety of the relevant code is provided for a comprehensive assessment. You may also include comments or annotations to better explain key sections of the code. Example: Code snippet for sorting algorithm requiring optimization.</p>
-            @if ($errors->has('code'))
-                @foreach ($errors->get('code') as $error)
-                    <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $error }}</p>
-                @endforeach
-            @endif
+            <x-input-error for="code" class="mt-2" />
         </fieldset>
     </div>
 
@@ -58,16 +42,12 @@
         <fieldset>
             <div class="flex">
                 <div class="flex items-center h-5">
-                    <input id="is_public" name="is_public" aria-describedby="is_public-text" type="checkbox" value="1" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" @if (old('is_public') === "1") checked @endif>
+                    <x-checkbox id="is_public" name="is_public" aria-describedby="is_public-text" :value="1" @if (old('is_public') === "1") checked @endif />
                 </div>
                 <div class="ms-2 text-sm">
-                    <label for="is_public" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Code Visibility</label>
+                    <x-label for="is_public" value="Code Visibility" />
                     <p id="is_public-text" class="text-xs font-normal text-gray-500 dark:text-gray-300">Indicate the visibility status of the code. Check if the code is accessible to everyone or uncheck if it's restricted to a specific team</p>
-                    @if ($errors->has('is_public'))
-                        @foreach ($errors->get('is_public') as $error)
-                            <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $error }}</p>
-                        @endforeach
-                    @endif
+                    <x-input-error for="is_public" class="mt-2" />
                 </div>
             </div>
         </fieldset>
