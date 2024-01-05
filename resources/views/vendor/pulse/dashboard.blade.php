d3737c759f6151dcbf1ed641c454d68729d2fce9
jean-marc strauven <jms@grazulex.be>
2024-01-05 06:07:14 +0100

    install pulse

---
+++ resources/views/vendor/pulse/dashboard.blade.php
@@ -0,0 +1,19 @@
+<x-pulse>
+    <livewire:pulse.servers cols="full" />
+
+    <livewire:pulse.usage cols="4"  />
+
+    <livewire:pulse.queues cols="8" rows="2" />
+
+    <livewire:pulse.cache cols="4" />
+
+    <livewire:pulse.slow-queries cols="4" />
+
+    <livewire:pulse.exceptions cols="4" />
+
+    <livewire:pulse.slow-requests cols="4" />
+
+    <livewire:pulse.slow-jobs cols="6" />
+
+    <livewire:pulse.slow-outgoing-requests cols="6" />
+</x-pulse>
