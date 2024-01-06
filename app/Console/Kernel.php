b1c2c38d849db19d40a9dfecc9b1d8f1c0910c7a
Jean-Marc Strauven <jms@grazulex.be>
2024-01-06 11:24:02 +0100

    add metric horizon

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ final class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
     /**
