972278d7f285fb58546dbc918351564f750aaa24
Denver <alagorn21@mail.ru>
2024-01-17 18:55:37 +0300

    PC-3 add: dashboard style

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-[var(--primary)] bg-[var(--light-primary)] dark:border-[var(--gray-smoke)] text-white dark:bg-[#182F44] dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-[var(--primary)] bg-[var(--light-primary)] dark:border-[var(--gray-smoke)] dark:text-white dark:bg-[#182F44] dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
