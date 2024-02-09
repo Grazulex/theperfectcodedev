ca21f1bb205fe9b65031b4427390df4689853c89
Denver <alagorn21@mail.ru>
2024-02-09 17:44:48 +0300

    PC-10 add: style page

--- resources/views/components/form-section.blade.php
+++ resources/views/components/form-section.blade.php
@@ -1,6 +1,6 @@
 @props(['submit'])
 
-<div {{ $attributes->merge(['class' => 'max-w-4xl']) }}>
+<div {{ $attributes->merge(['class' => 'max-w-4xl bg-white dark:bg-[var(--dark)]']) }}>
     <x-section-title>
         <x-slot name="title">{{ $title }}</x-slot>
         <x-slot name="description">{{ $description }}</x-slot>
@@ -8,7 +8,8 @@
 
     <div class="mt-5 md:mt-0 md:col-span-2">
         <form wire:submit="{{ $submit }}">
-            <div class="px-4 py-5 bg-white dark:bg-[var(--dark)] sm:p-6 shadow {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
+            <x-section-border />
+            <div class="px-4 pb-5 bg-white dark:bg-[var(--dark)] sm:p-6 sm:pt-0 shadow {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
                 <div class="grid grid-cols-6 gap-6">
                     {{ $form }}
                 </div>
--- resources/views/components/section-title.blade.php
+++ resources/views/components/section-title.blade.php
@@ -1,4 +1,4 @@
-<div class="flex justify-between mb-6 md:col-span-1">
+<div class="flex justify-between px-4 pt-6 sm:px-6 md:col-span-1 ">
     <div class="flex">
         <h3 class="mr-2 text-lg font-medium text-gray-900 dark:text-gray-100">{{ $title }}:</h3>
         <p class="mt-1 text-base text-gray-600 dark:text-gray-400">
