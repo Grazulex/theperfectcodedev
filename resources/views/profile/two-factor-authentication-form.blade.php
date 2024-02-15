5886b0f6d1a3bf09c45c477ded6c3790ec95ca31
Denver <alagorn21@mail.ru>
2024-02-15 18:36:07 +0300

    PC-13 add: fix  btn style

--- resources/views/profile/two-factor-authentication-form.blade.php
+++ resources/views/profile/two-factor-authentication-form.blade.php
@@ -62,13 +62,13 @@
             @endif
 
             @if ($showingRecoveryCodes)
-                <div class="max-w-xl mt-4 text-sm text-gray-600 dark:text-gray-400">
-                    <p class="font-semibold">
+                <div class="mt-3 text-sm text-gray-600 dark:text-gray-400">
+                    <p>
                         {{ __('Store these recovery codes in a secure password manager. They can be used to recover access to your account if your two factor authentication device is lost.') }}
                     </p>
                 </div>
 
-                <div class="grid max-w-xl gap-1 px-4 py-4 mt-4 font-mono text-sm bg-gray-100 rounded-lg dark:bg-gray-900 dark:text-gray-100">
+                <div class="grid w-full gap-1 px-4 py-4 mt-4 font-mono text-sm bg-gray-100 rounded-lg dark:bg-gray-900 dark:text-gray-100">
                     @foreach (json_decode(decrypt($this->user->two_factor_recovery_codes), true) as $code)
                         <div>{{ $code }}</div>
                     @endforeach
