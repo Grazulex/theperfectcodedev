860e5a573ac9f2185b1642ad822f0edf4c4cfa13
Denver <alagorn21@mail.ru>
2024-02-15 18:47:17 +0300

    PC-13 fix: fix

--- resources/views/components/form-section.blade.php
+++ resources/views/components/form-section.blade.php
@@ -6,7 +6,7 @@
         <x-slot name="description">{{ $description }}</x-slot>
     </x-section-title>
 
-    <div class="mt-5 md:mt-0 md:col-span-2">
+    <div class="pb-3 mt-5 md:mt-0 md:col-span-2">
         <form wire:submit="{{ $submit }}">
             <x-section-border />
             <div class="px-4 pb-5 bg-white dark:bg-[var(--dark)] sm:p-6 sm:pt-0 shadow {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
