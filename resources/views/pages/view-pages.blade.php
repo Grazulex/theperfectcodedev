d9bbc445f34ef070a6aba388a00198f5f7238aae
Jean-Marc Strauven <jms@grazulex.be>
2024-02-16 16:15:48 +0100

    fix issue with profile picture in view page

--- resources/views/pages/view-pages.blade.php
+++ resources/views/pages/view-pages.blade.php
@@ -42,11 +42,23 @@
                 <div class="overflow-hidden bg-white shadow-xl dark:bg-gray-800 sm:rounded-lg md:basic-1/5">
                     <div class="flex justify-between w-full border-b border-[#3A445B]">
                         <div class="flex">
-                            <button class="flex p-2 text-sm transition border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300">
-                                <img class="object-cover w-8 h-8 rounded-full" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                            </button>
+                            @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                                <button class="flex p-2 text-sm transition border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300">
+                                    <img class="object-cover w-8 h-8 rounded-full" src="{{ $pageArray['user']['profile_photo_url'] }}" alt="{{ $pageArray['user']['name'] }}" />
+                                </button>
+                            @else
+                                <span class="inline-flex rounded-md">
+                                        <button type="button" class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md dark:text-gray-400 dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700">
+                                            {{ $pageArray['user']['name'] }}
+
+                                            <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                            </svg>
+                                        </button>
+                                    </span>
+                            @endif
                             <div class="self-center">
-                                <x-label class="truncate max-w-[100px] dark:text-[white] text-gray-800" for="name" value="{{ __('Jean-marc Strauven') }}" />
+                                <x-label class="truncate max-w-[100px] dark:text-[white] text-gray-800" for="name" value="{{ $pageArray['user']['name'] }}" />
                                 @if (Auth::check() && Auth::user()->id === $pageArray['user']['id'])
                                     <livewire:pages.others :is_public="$pageArray['is_public']"/>
                                 @endif
