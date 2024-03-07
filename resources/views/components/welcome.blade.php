dd4df15427204e76b07002deee95700b443e91e4
Denver <alagorn21@mail.ru>
2024-03-07 19:21:59 +0300

    PC-14 add: delete

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,11 +1,10 @@
-<div class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
-    <x-application-logo class="block h-12 w-auto" />
+<div class="p-6 bg-white border-b border-gray-200 lg:p-8 dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent dark:border-gray-700">
 
     <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
         Welcome to The Perfect Code Dev project!
     </h1>
 
-    <p class="mt-6 text-gray-500 dark:text-gray-400 leading-relaxed">
+    <p class="mt-6 leading-relaxed text-gray-500 dark:text-gray-400">
         Welcome to our platform where code collaboration meets the pursuit of perfection. Here, we invite you to share your code and harness the collective expertise of our community to refine it further.
         <br/>
         Our goal is simple: to create an environment where every visitor becomes a contributor, sharing their code and insights to collectively craft the perfect solution. Whether you're a seasoned developer or just starting, your input matters.
@@ -14,18 +13,18 @@
     </p>
 </div>
 
-<div class="bg-gray-200 dark:bg-gray-800 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
+<div class="grid grid-cols-1 gap-6 p-6 bg-gray-200 bg-opacity-25 dark:bg-gray-800 md:grid-cols-2 lg:gap-8 lg:p-8">
     <div>
         <div class="flex items-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
             </svg>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
+            <h2 class="text-xl font-semibold text-gray-900 ms-3 dark:text-white">
                 <a href="https://laravel.com/docs">Community-driven Refinement</a>
             </h2>
         </div>
 
-        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+        <p class="mt-4 text-sm leading-relaxed text-gray-500 dark:text-gray-400">
             By leveraging a community of developers, the system encourages collaborative improvement. This collective expertise results in a diverse range of perspectives and insights, leading to refined and high-quality code.
         </p>
 
@@ -36,12 +35,12 @@
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
                 <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
             </svg>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
+            <h2 class="text-xl font-semibold text-gray-900 ms-3 dark:text-white">
                 <a href="https://laracasts.com">Continuous Iteration and Enhancement</a>
             </h2>
         </div>
 
-        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+        <p class="mt-4 text-sm leading-relaxed text-gray-500 dark:text-gray-400">
             Through the platform's interactive nature, code undergoes constant iterations. This perpetual refining process ensures that code stays updated, optimized, and aligned with evolving industry standards.
         </p>
 
@@ -52,12 +51,12 @@
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
             </svg>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
+            <h2 class="text-xl font-semibold text-gray-900 ms-3 dark:text-white">
                 <a href="https://tailwindcss.com/">Diverse Feedback and Insights</a>
             </h2>
         </div>
 
-        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+        <p class="mt-4 text-sm leading-relaxed text-gray-500 dark:text-gray-400">
             Users receive feedback and suggestions from a broad spectrum of developers. This diverse input not only enhances the code's quality but also fosters learning opportunities and innovative approaches.
         </p>
     </div>
@@ -67,12 +66,12 @@
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
             </svg>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
+            <h2 class="text-xl font-semibold text-gray-900 ms-3 dark:text-white">
                 Accelerated Problem-solving
             </h2>
         </div>
 
-        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+        <p class="mt-4 text-sm leading-relaxed text-gray-500 dark:text-gray-400">
             With a pool of contributors sharing their expertise, challenges are met with multiple solutions. This accelerates problem-solving, enabling faster resolution of coding issues and the discovery of more efficient approaches.
         </p>
     </div>
