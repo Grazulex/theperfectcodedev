74aff4c3014fb43ccf9b60de0c76b38fab11e33d
Denver <alagorn21@mail.ru>
2024-04-10 17:24:12 +0300

    PC-14 add: style edit

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div  class="py-12">
+    <div  class="pb-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-xl dark:bg-gray-800 sm:rounded-lg">
                 <x-welcome />
