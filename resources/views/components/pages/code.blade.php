527b5da280bd78c678465476b5f0d2d0dba066b3
Denver <alagorn21@mail.ru>
2024-04-12 13:41:15 +0300

    PC-14 add: fix code colors

--- resources/views/components/pages/code.blade.php
+++ resources/views/components/pages/code.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="mb-8 prose text-gray-800 dark:text-gray-200 lg:prose-l max-w-none">
+    <div class="mb-8 prose text-gray-800 text-color dark:text-gray-200 lg:prose-l max-w-none">
         {!! Str::markdown($code) !!}
     </div>
 </div>
